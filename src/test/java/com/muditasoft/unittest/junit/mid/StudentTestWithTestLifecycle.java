3a172d0c8d0145cc46776ae45208bbd2df7bc6ae
tutkuince <tutku.ince@outlook.com>
2020-08-31 21:55:49 +0300

    StudentTestWithTestLifecycle

---
+++ src/test/java/com/muditasoft/unittest/junit/mid/StudentTestWithTestLifecycle.java
@@ -0,0 +1,30 @@
+package com.muditasoft.unittest.junit.mid;
+
+import com.muditasoft.unittest.courserecord.Student;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class StudentTestWithTestLifecycle {
+
+    private Student jack = new Student("1", "Jack", "Ince");
+
+    @BeforeAll
+    static void beforeAll() {
+
+    }
+
+    @Test
+    void state_cannot_change_when_lifecycle_is_per_method() {
+        assertEquals("Jack", jack.getName());
+        jack = new Student("2", "Alex", "De Souza");
+    }
+
+    @Test
+    void state_can_change_when_lifecycle_is_per_class() {
+        assertEquals("Jack", jack.getName());
+        jack = new Student("2", "Alex", "De Souza");
+    }
+}
