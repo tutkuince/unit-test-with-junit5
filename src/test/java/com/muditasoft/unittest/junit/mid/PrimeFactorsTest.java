6a21623f32c1e71d5792821de1c2df4b6985d360
tutkuince <tutku.ince@outlook.com>
2020-09-01 22:24:49 +0300

    generate_with_repeated_test

--- src/test/java/com/muditasoft/unittest/junit/mid/PrimeFactorsTest.java
+++ src/test/java/com/muditasoft/unittest/junit/mid/PrimeFactorsTest.java
@@ -1,14 +1,31 @@
 package com.muditasoft.unittest.junit.mid;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PrimeFactorsTest {
+
+    private Map<Integer, List<Integer>> primeFactorsExpectations = new HashMap<>();
+
+    @BeforeAll
+    void beforeAll() {
+        primeFactorsExpectations.put(1, Collections.EMPTY_LIST);
+        primeFactorsExpectations.put(2, List.of(2));
+        primeFactorsExpectations.put(3, List.of(3));
+        primeFactorsExpectations.put(4, List.of(2, 2));
+        primeFactorsExpectations.put(5, List.of(5));
+        primeFactorsExpectations.put(6, List.of(2, 3));
+        primeFactorsExpectations.put(7, List.of(7));
+        primeFactorsExpectations.put(8, List.of(2, 2, 2));
+        primeFactorsExpectations.put(9, List.of(3, 3));
+    }
 
     @Test
     @DisplayName("Generate Prime Factors")
@@ -22,5 +39,11 @@ class PrimeFactorsTest {
         assertEquals(List.of(7), PrimeFactors.generate(7));
         assertEquals(List.of(2, 2, 2), PrimeFactors.generate(8));
         assertEquals(List.of(3, 3), PrimeFactors.generate(9));
+    }
+
+    @RepeatedTest(9)
+    void generate_with_repeated_test(RepetitionInfo repetitionInfo) {
+        assertEquals(primeFactorsExpectations.get(repetitionInfo.getCurrentRepetition()),
+                PrimeFactors.generate(repetitionInfo.getCurrentRepetition()));
     }
 }
